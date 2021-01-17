607862bb93e010bcea53e3850c74423690d54cb9
Danilson Sanches <dsanches06@outlook.com>
2021-01-17 18:25:59 +0000

    criar fumção jump para permitir que o dinossauro salta ao pressionar o espaço

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,33 @@
 const dino = document.querySelector(".dino");
 
 function handleKeyUP(event) {
-  if (event.keyCode === 38) {
-      console.log("Pressionou espaço")
+  if (event.keyCode === 32) {
+    jump();
   }
+}
+
+function jump() {
+  let position = 0;
+  let upInterval = setInterval(() => {
+    if (position >= 150) {
+      //resetar intervalo
+      clearInterval(upInterval);
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          //resetar intervalo
+          clearInterval(downInterval);
+        } else {
+          //Descendo
+          position -= 20;
+          dino.style.bottom = position + "px";
+        }
+      }, 20);
+    } else {
+      //subindo
+      position += 20;
+      dino.style.bottom = position + "px";
+    }
+  }, 20);
 }
 
 document.addEventListener("keyup", handleKeyUP);
