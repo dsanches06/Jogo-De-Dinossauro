29414f5acc0ac2c2f21c88e3fe58a64ea86a2543
Danilson Sanches <dsanches06@outlook.com>
2021-01-17 18:46:51 +0000

    criar cactus

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,26 @@
 const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
+let isJumping = false;
 
+/**
+ *  Função que captura o evento do teclado ao ser pressionado
+ */
 function handleKeyUP(event) {
   if (event.keyCode === 32) {
-    jump();
+    if (!isJumping) {
+      jump();
+    }
   }
 }
 
+/**
+ *  Função jump que pernite o dinossauro saltar
+ */
 function jump() {
+  //em baixo
   let position = 0;
+  isJumping = true;
+
   let upInterval = setInterval(() => {
     if (position >= 150) {
       //resetar intervalo
@@ -16,6 +29,7 @@ function jump() {
         if (position <= 0) {
           //resetar intervalo
           clearInterval(downInterval);
+          isJumping = false;
         } else {
           //Descendo
           position -= 20;
@@ -30,4 +44,23 @@ function jump() {
   }, 20);
 }
 
+/**
+ * Função que cria o cactus
+ */
+function createCactus() {
+  const cactus = document.createElement("div");
+  //na direita
+  let position = 1000;
+  //adicionar uma class ao div
+  cactus.classList.add("cactus");
+  //posicionar na direita
+  cactus.style.left = 1000 + "px";
+  //adicionar um elemento html
+  background.appendChild(cactus);
+}
+
+/**
+ * Funções
+ */
+createCactus();
 document.addEventListener("keyup", handleKeyUP);
